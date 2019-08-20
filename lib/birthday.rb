f6b931b821c036a558ed3e53bd04aaac4f1c12d4
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-20 21:44:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,6 +10,12 @@ def happy_birthday(birthday_kids)
   end 
 end
 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kid, age|
+    if age <= 12 
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+end 
+
 
 
 
